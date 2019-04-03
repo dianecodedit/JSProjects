1e95dc01b0a97d0d9017e1c494fd5810b51fcb71
dianecodedit <diane@codedit.tech>
2019-04-03 19:28:02 +1300

    To Do App added

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,8 +1,32 @@
-const stickyNote1 = document.getElementById("sn1")
-const stickyNote2 = document.getElementById("sn2")
-const stickyNote3 = document.getElementById("sn3")
-const stickyNote4 = document.getElementById("sn4")
-const stickyNote1Header = document.getElementById("sn1header")
-const stickyNote2Header = document.getElementById("sn2header")
-const stickyNote3Header = document.getElementById("sn3header")
-const stickyNote4Header = document.getElementById("sn4header")
+const stickyNote1 = document.getElementById("toDoInput1")
+const stickyNote2 = document.getElementById("toDoInput2")
+const stickyNote3 = document.getElementById("toDoInput3")
+const stickyNote4 = document.getElementById("toDoInput4")
+const ul1 = document.getElementById("ul1")
+const ul2 = document.getElementById("ul2")
+const ul3 = document.getElementById("ul3")
+const ul4 = document.getElementById("ul3")
+const li = document.createElement("li");
+
+
+let taskList1 = []
+let tasklist2 = []
+let tasklist3 = []
+let tasklist4 = []
+
+
+stickyNote1.addEventListener("keypress", addItem1)
+
+function addItem1(e) {
+  let task = stickyNote1.value
+  if (e.keyCode == 13) {
+    if (taskList1.length <= 6) {
+      taskList1.push(task)
+      let text = document.createTextNode(task)
+      li.appendChild(text)
+      ul1.appendChild(li)
+    }
+  }
+
+
+}
